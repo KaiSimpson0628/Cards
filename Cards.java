fb97a97aab4875af0fc1361539bec16d29615402
KaiSimpson0628 <60585848+KaiSimpson0628@users.noreply.github.com>
2022-02-25 09:30:14 -0500

    Create Cards.java

---
+++ Cards.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Cards {
+
+/*deck of cards
+52 cards
+4 sets of 13 faces
+4 suites of 13 ranks
+ String suit = {"hearts", "diamonds", "spades", "clubs"}
+String ranks = {"2", "3", ...
+Card object
+ suit
+ rank
+ int value 
+*/
+int deckSize = 52;
+String[] suit = {"hearts", "diamonds", "spades", "clubs"};
+String[] ranks = {"2", "3", "4", "5"};
+	int[] value = {1, 2, 3, 4};
+	
+	for (int i = 0; i<=4; i++) {
+		for (int o = 0; o<=4; o++) { 
+			String[] suit 
+			
